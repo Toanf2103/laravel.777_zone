2a13cfe62dc602b54fdb2cff04ddb722803137df
Toanf2103 <nobitakute002@gmail.com>
2023-11-22 14:08:02 +0700

    fix http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Brand;
 use App\Models\Category;
+use Illuminate\Routing\UrlGenerator as RoutingUrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(RoutingUrlGenerator $url): void
     {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
         $views = ['site.pages.*', 'admin.pages.product.*', 'admin.pages.banner.*'];
         foreach ($views as $view) {
             view()->composer($view, function ($view) {
